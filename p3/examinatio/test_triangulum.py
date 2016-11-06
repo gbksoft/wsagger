c994faf8760d4ddc5745517f0616eddec976e11c
Pavlo Dzikovsky <p.dzikovsky@gmail.com>
2016-11-06 23:19:00 +0200

    schema & validator are corrected

--- p3/examinatio/test_triangulum.py
+++
@@ -1,21 +0,0 @@
-import unittest
-
-if __name__ == '__main__':
-   import sys
-   sys.path.append ('C:/!pavlo/P/')
-   sys.path.append ('C:/!pavlo/C/python_calculatio/')
-
-from   mathematica  import *
-from   triangulum   import *
-
-import cura.examinatio
-
-indiligentia = 0.0001
-
-class Scaenarium (basis.examinatio.Scaenarium):
-   def Test_triangulum_ab_dij (self):
-      exitus = Triangulum_ab_dij (*self.data)
-      self.assertFalse (apprecia_errorem (exitus, self.exitus, indiligentia, *self.data))
-
-if __name__ == '__main__':
-   cura.examinatio.Examina_locum ('.', sys.argv[1:])
