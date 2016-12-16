d27c4912148220761a80893d8c8dfb3c3c87394d
pavlo <p.dzikovsky@gmail.com>
2016-12-16 18:39:34 +0200

    1216

--- convert/1.js
+++
@@ -1,8 +0,0 @@
-
-var d = '{a}saert {bc} saer {a}'
-var p = { a: '{{a}}', bc: '{{bc}}' };
-
-d = d.replace(/\{[^}]*\}/g, (x) => { x.substr(1, x.length - 2); return (x in p) ? p[x] : '{' + x + '}'; }); 
-
-
-console.log(d);
