55359bf0e5a24a22a2a77240deb43b4157c8c57c
pavlo <p.dzikovsky@gmail.com>
2017-01-11 19:01:50 +0200

    socket.io example

---
+++ examples/socket-server/start.js
@@ -0,0 +1,33 @@
+var app = require('http').createServer()
+var io  = require('socket.io')(app);
+
+app.listen(10002);
+console.log ('listening on :10002')
+
+/*
+var fs = require('fs');
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+*/
+
+io.on('connection', (socket) => {
+  socket.emit('connectedTo', { socketId: socket.id });
+  // socket.on('error', console.log);
+  
+  socket.on('message', (data) => {
+     socket.emit('message',  data);
+     socket.emit('message_', data);
+  
+     console.log('socket.on:', 'message', data);
+  });
+});
